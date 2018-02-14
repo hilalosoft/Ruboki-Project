bd0ef7a40d60e972f7832de384ed32b7dcf99c0d
hilal <hialtaha@Hotmail.com>
2018-02-14 18:42:09 +0100

    lol

--- src/think.py
+++ src/think.py
@@ -225,7 +225,7 @@ def decide(status):
 
 	publisher_velocity.publish(forward)
 	outfile=open("position.txt","a")
-	if len(numchar!=0):
+	if len(numchar)!=0:
 		count+=len(repr(status.x))+len(repr(status.y))+len(",forward")+len("\n")+numchar[len(numchar)-1]
 	else:
 		count+=len(repr(status.x))+len(repr(status.y))+len(",forward")+len("\n")
