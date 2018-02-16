bef459549537951e2881f75fd01085dda84c5fda
hilalosoft <hilaltaha@hotmail.com>
2018-02-16 15:18:54 +0100

    Update think.py

--- src/think.py
+++ src/think.py
@@ -12,8 +12,8 @@ import atexit
 
 publisher_velocity = rospy.Publisher('kobuki_velocity', Twist, queue_size=1)
 #publisher_velocity = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=1)
-forward = Twist(Vector3(0.4,0,0),Vector3(0,0,0))
-backward = Twist(Vector3(-0.4,0,0),Vector3(0,0,0))
+forward = Twist(Vector3(0.2,0,0),Vector3(0,0,0))
+backward = Twist(Vector3(-0.2,0,0),Vector3(0,0,0))
 backward_obst = Twist(Vector3(-0.1,0,0),Vector3(0,0,0))
 right_spin = Twist(Vector3(0,0,0),Vector3(0,0,-1.5))
 left_spin = Twist(Vector3(0,0,0),Vector3(0,0,1.5))
